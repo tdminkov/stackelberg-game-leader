5e57511f7174c31576eb1da2c3675d71f2f7da40
Carlos <carlos.pinavazgomes@student.manchester.ac.uk>
2019-03-30 12:12:12 +0000

    partial model train

--- Model.java
+++ Model.java
@@ -25,6 +25,11 @@ public class Model {
     // sum of squared leader price * sum of follower reaction
     // - sum of leader price * sum of (leader price * follower reaction)
     // / number of data points * sum of squared leader price - sum of leader price squared
+
+    // b
+    // number of data points * sum of (leader price * follower reaction)
+    // - sum of leader price * sum of follower reaction
+    // / number of data points * sum of squared leader price - sum of leader price squared
     float sum_squared_leader = 0f;
     float sum_leader = 0f;
     float sum_leader_times_follower = 0f;
@@ -39,11 +44,18 @@ public class Model {
     	public final float m_followerPrice;
       // The cost of the follower
     	public final float m_cost; */
+
+      sum_leader += record.m_leaderPrice;
+      sum_squared_leader += record.m_leaderPrice * record.m_leaderPrice;
+      sum_follower += record.m_followerPrice;
+      sum_leader_times_follower += record.m_leaderPrice * record.m_followerPrice;
     }
 
+    a = sum_squared_leader * sum_follower
+    - sum_leader * sum_leader_times_follower
+    / (records.size() * sum_squared_leader - (sum_leader*sum_leader))
 
-    // b
-    // number of data points * sum of (leader price * follower reaction)
+    b = records.size() * sum of (leader price * follower reaction)
     // - sum of leader price * sum of follower reaction
     // / number of data points * sum of squared leader price - sum of leader price squared
 
